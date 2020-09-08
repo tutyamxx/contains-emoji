320315df98389afdc1c587ebe83a24c998d5fbdd
tutyamxx <tutygoo@gmail.com>
2020-09-08 17:00:49 +0100

    Initial upload

---
+++ index.js
@@ -0,0 +1,19 @@
+
+/**
+ *  has-emoji - Checks if a string contains one or more emojis 👀
+ *  @version: v1.0.0
+ *  @link: https://github.com/tutyamxx/contains-emoji
+ *  @license: MIT
+ **/
+const EmojiRegex = require("emoji-regex");
+const CreateEmojiRegex = EmojiRegex();
+
+module.exports = (szString) =>
+{
+    if(CreateEmojiRegex.test(szString))
+    {
+        return true;
+    }
+
+    return false;
+};

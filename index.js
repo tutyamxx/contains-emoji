a480057c35d6cc8b4b97979dd1db2a7854c158ac
Alexandru Maxim <15928925+tutyamxx@users.noreply.github.com>
2021-10-05 17:50:17 +0100

    FIX: Wording in tests

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
  *  contains-emoji - Checks if a string contains one or more emojis 👀
- *  @version: v1.0.8
+ *  @version: v1.0.9
  *  @link: https://github.com/tutyamxx/contains-emoji
  *  @license: MIT
  **/
--- test/index.test.js
+++ test/index.test.js
@@ -6,13 +6,13 @@ test("Should return true if string contains one or more emojis", () => {
     expect(containsEmoji("Checks if a string contains one or more emojis 👀")).toBe(true);
 });
 
-test("Should return false if string contains one or more emojis", () => {
+test("Should return false if string does not contain any emojis", () => {
     expect(containsEmoji("I ate a slice of cake last night.")).toBe(false);
     expect(containsEmoji("This company makes amazing cakes. I took a slice, it was delicious!")).toBe(false);
     expect(containsEmoji("Checks if a string contains one or more emojis")).toBe(false);
 });
 
-test("Should return false if there is an empty string", () => {
+test("Should return false if there is an empty string or nothing passed as parameter", () => {
     expect(containsEmoji("")).toBe(false);
     expect(containsEmoji()).toBe(false);
     expect(containsEmoji(" ")).toBe(false);

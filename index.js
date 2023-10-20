61f970124a81ffa9f0d54d18c5a781715ce35b16
tutyamxx <15928925+tutyamxx@users.noreply.github.com>
2023-10-20 18:13:12 +0100

    UPDATE: Updated workflow file and changed version

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
  *  contains-emoji - Checks if a string contains one or more emojis 👀
- *  @version: v1.1.5
+ *  @version: v1.1.6
  *  @link: https://github.com/tutyamxx/contains-emoji
  *  @license: MIT
  **/

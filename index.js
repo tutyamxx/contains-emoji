63b70350a6ed185209b85ca95d655bd271923980
tutyamxx <15928925+tutyamxx@users.noreply.github.com>
2025-04-24 17:24:19 +0100

    UPDATE: Optimised for better and faster performance

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 /**
  *  contains-emoji - Checks if a string contains one or more emojis 👀
- *  @version: v1.1.9
+ *  @version: v1.2.0
  *  @link: https://github.com/tutyamxx/contains-emoji
  *  @license: MIT
  **/
 
 const emojiRegex = require('emoji-regex');
+const emojiRegexPatternCache = emojiRegex();
 
-module.exports = (paramString) => (paramString && paramString.length > 0 && typeof paramString === 'string') ? (!!emojiRegex().test(paramString)) : false;
+module.exports = (paramString) => {
+    if (paramString == null || typeof paramString !== 'string') {
+        return false;
+    }
+    return paramString.length > 0 && emojiRegexPatternCache.test(paramString);
+};

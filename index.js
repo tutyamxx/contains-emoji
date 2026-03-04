52db56f5daf95ec276db48f739f1234dbd8b55f8
tutyamxx <15928925+tutyamxx@users.noreply.github.com>
2026-03-04 14:09:17 +0000

    UPDATE: Fixed vulnerabilities and updated CI file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
  *  contains-emoji - Checks if a string contains one or more emojis 👀
- *  @version: v1.3.1
+ *  @version: v1.3.2
  *  @link: https://github.com/tutyamxx/contains-emoji
  *  @license: MIT
  **/
